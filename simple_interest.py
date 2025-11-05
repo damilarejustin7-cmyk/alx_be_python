33319c35b065322f7c04e98e5de0df60361e605a
Damilare Adegbola <damilarejustin7@gmail.com>
2025-11-05 11:33:49 +0100

    Calculated simple interest

---
+++ simple_interest.py
@@ -0,0 +1,6 @@
+Principal = 1000  # Initial amount of money
+Rate = 0.05     # Annual interest rate (5%)
+Time = 3        # Time in years
+# Calculate simple interest
+Simple_Interest = Principal * Rate * Time
+print("Simple Interest:", Simple_Interest)
