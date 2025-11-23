5ec976a4942fa6e6433038e1fa7a660fc69f1c00
Damilare Adegbola <damilarejustin7@gmail.com>
2025-11-23 12:42:55 +0100

    Deleted and rebuilt the fns_and_dsa subfolder structure

--- control-flow/python_introduction/hours_to_seconds.py
+++
@@ -1,3 +0,0 @@
-hours = 2 # Number or hour to be converted to seconds
-seconds = hours * 3600
-print (hours, "hour(s) is", seconds, "seconds.")
--- control-flow/python_introduction/rectangle_area.py
+++
@@ -1,5 +0,0 @@
-length = 10 # Length of the rectangle
-width = 5   # Width of the rectangle
-# Calculate the area of the rectangle
-area = length * width
-print("The area of the rectangle is:", area)
