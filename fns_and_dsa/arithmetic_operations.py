47929c1d9a2206887f51e29c92a7dfb509db4895
Damilare Adegbola <damilarejustin7@gmail.com>
2025-11-23 13:59:00 +0100

    Finished task

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,6 +1,6 @@
-# Simple arithmetic operations 
+# arithmetic_operations.py
 
-def perform_operation(num1: float, num2: float, operation: str):
+def perform_operation(num1, num2, operation):
     if operation == 'add':
         return num1 + num2
     elif operation == 'subtract':
@@ -9,8 +9,8 @@ def perform_operation(num1: float, num2: float, operation: str):
         return num1 * num2
     elif operation == 'divide':
         if num2 == 0:
-            return "Error: Division by zero"
+            return None
         return num1 / num2
     else:
-        return "Error: Invalid operation"
+        return None
 
