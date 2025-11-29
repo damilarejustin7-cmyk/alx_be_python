0e7e12c23a32921b04741ad3eadd32a7e222f470
Damilare Adegbola <damilarejustin7@gmail.com>
2025-11-29 12:52:15 +0100

    Fix code to pass task 1 checks

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -4,8 +4,11 @@ def safe_divide(numerator, denominator):
     try:
         num = float(numerator)
         denom = float(denominator)
+
         if denom == 0:
             return "Error: Cannot divide by zero."
-        return num / denom
+
+        result = num / denom
+        return f"The result of the division is {result}"
     except ValueError:
-        return "Error: Please enter numeric values only."   
+        return "Error: Please enter numeric values only."
