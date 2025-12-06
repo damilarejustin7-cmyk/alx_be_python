f733d06110e0dc7b5a37c93867c06492f6ed4699
Damilare Adegbola <damilarejustin7@gmail.com>
2025-12-06 13:21:02 +0100

    Fixed 2 line of codes in Task 0

--- oop/book_class.py
+++ oop/book_class.py
@@ -7,12 +7,12 @@ class Book:
         self.year = year
 
     def __str__(self):
-            return f"'{self.title}' by {self.author})"
+            return f"{self.title} by {self.author} published in {self.year}"
 
     def __repr__(self):
             return f"Book(title='{self.title}', author='{self.author}', year={self.year})"
 
     def __del__(self):
-            print(f"Book '{self.title}' is being deleted.") 
+            print(f"Deleting {self.title}") 
                 
 
