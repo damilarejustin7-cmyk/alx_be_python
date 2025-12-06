0acee50efb657adb223f001ce73a3c1b4d7b7c14
Damilare Adegbola <damilarejustin7@gmail.com>
2025-12-06 14:14:41 +0100

    Fixed another line of code in task 1

--- oop/library_system.py
+++ oop/library_system.py
@@ -1,29 +1,33 @@
-# Simple Library System mastering inheritance and composition
-
+# Simple library system showing mastering or inheritance and composition
 class Book:
     def __init__(self, title, author):
         self.title = title
         self.author = author
 
     def __str__(self):
-        return f"{self.title} by {self.author}"    
+        return f"Book: {self.title} by {self.author}"
+
 
 class EBook(Book):
-    def __init__ (self,title,author,file_size):
+    def __init__(self, title, author, file_size):
         super().__init__(title, author)
         self.file_size = file_size  # in KB
 
-    def __str__(self):   
-        return f"{self.title} by {self.author}, Size: {self.file_size}KB"
+    def __str__(self):
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
 
-class PrintBook(Book):        
-    def __init__(self,title,author,page_count):
+
+class PrintBook(Book):
+    def __init__(self, title, author, page_count):
         super().__init__(title, author)
         self.page_count = page_count  # number of pages
 
     def __str__(self):
-        return f"print {self.title} by {self.author}, Pages: {self.page_count}"
-    
+        return (
+            f"PrintBook: {self.title} by {self.author}, "
+            f"Page Count: {self.page_count}"
+        )
+
 class Library:
     def __init__(self):
         self.books = []
@@ -33,4 +37,4 @@ class Library:
 
     def list_books(self):
         for book in self.books:
-            print(book)   # uses each object's __str__()
+            print(book)
