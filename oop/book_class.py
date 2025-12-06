2c1f705b9bfddeaaf7013c1dcd8533c34236148e
Damilare Adegbola <damilarejustin7@gmail.com>
2025-12-06 13:11:30 +0100

    Just finished Task one

---
+++ oop/book_class.py
@@ -0,0 +1,18 @@
+# magic methods in a Book class
+
+class Book:
+    def __init__(self, title, author, year):
+        self.title = title
+        self.author = author
+        self.year = year
+
+    def __str__(self):
+            return f"'{self.title}' by {self.author})"
+
+    def __repr__(self):
+            return f"Book(title='{self.title}', author='{self.author}', year={self.year})"
+
+    def __del__(self):
+            print(f"Book '{self.title}' is being deleted.") 
+                
+
