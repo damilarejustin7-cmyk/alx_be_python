5bde45696dedac104305ee7ae0a8741eb8faf642
Damilare Adegbola <damilarejustin7@gmail.com>
2025-12-06 14:08:07 +0100

    Fixed several lines of codes in task 1

--- oop/library_system.py
+++ oop/library_system.py
@@ -5,16 +5,25 @@ class Book:
         self.title = title
         self.author = author
 
+    def __str__(self):
+        return f"{self.title} by {self.author}"    
+
 class EBook(Book):
     def __init__ (self,title,author,file_size):
         super().__init__(title, author)
         self.file_size = file_size  # in KB
+
+    def __str__(self):   
+        return f"{self.title} by {self.author}, Size: {self.file_size}KB"
 
 class PrintBook(Book):        
     def __init__(self,title,author,page_count):
         super().__init__(title, author)
         self.page_count = page_count  # number of pages
 
+    def __str__(self):
+        return f"print {self.title} by {self.author}, Pages: {self.page_count}"
+    
 class Library:
     def __init__(self):
         self.books = []
@@ -24,9 +33,4 @@ class Library:
 
     def list_books(self):
         for book in self.books:
-            if isinstance(book, EBook):
-                print(f"E-Book: {book.title} by {book.author}, Size: {book.file_size}KB")
-            elif isinstance(book, PrintBook):
-                print(f"Print Book: {book.title} by {book.author}, Pages: {book.page_count}")
-            else:
-                print(f"Book: {book.title} by {book.author}")        
+            print(book)   # uses each object's __str__()
